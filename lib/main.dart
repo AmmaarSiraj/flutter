c256caffc6cd1774bd10a8075fceb934b37e23d6
AmmaarSiraj <ammarsirajnomor1pl@gmail.com>
2024-06-05 11:26:14 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Toko Siraj",
+    home: new Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Toko Siraj"),
+      ),
+      body: new FutureBuilder<List>(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+
+          return snapshot.hasData
+              ? new ItemList(
+                  list: snapshot.data ?? [],
+                )
+              : new Center(
+                  child: new CircularProgressIndicator(),
+                );
+        },
+      ),
+    );
+  }
+
+  Future<List> getData() async {
+    final response = await http
+        .get(Uri.parse("http://localhost/latihan_Pmobile/getdata.php"));
+    return json.decode(response.body);
+  }
+}
+
+class ItemList extends StatelessWidget {
+  final List list;
+  const ItemList({super.key, required this.list});
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      itemCount: list == null ? 0 : list.length,
+      itemBuilder: (context, i) {
+        return new Text(list[i]['nama_item']);
+      },
+    );
+  }
+}
